6e26b94b35f0c0723f5c3284fda02bd1aa54d2e0
Alfex-1 <alexandrebrunet@free.fr>
2024-02-14 16:21:57 +0100

    Update temp series

--- Master 2/Séries temporelles/Code Python Prophet.py
+++ Master 2/Séries temporelles/Code Python Prophet.py
@@ -3,36 +3,28 @@ import pandas as pd
 from prophet import Prophet
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import numpy as np
-from datetime import datetime
+#from datetime import datetime
+import matplotlib.pyplot as plt
 
-#Data processing
+# Data processing
 df = pd.read_csv('valeurs_mensuelles.csv',sep=";")
 df.describe()
 df.columns=['ds','y']
 df['ds'] = pd.to_datetime(df['ds'])
 
+# Entraîner le modèle
 model = Prophet(interval_width=0.95)
 training_run = model.fit(df)
 
 future=model.make_future_dataframe(periods=25,freq='M')
 
-future
-
 forecast = model.predict(future)
 
-forecast.tail()
-
+# Graphiques de tendance et de saisonnalité annuelle
 plot1 = model.plot(forecast)
 
-plot2 = model.plot_components(forecast)
-
-
-# Restreindre la plage de dates pour les observations réelles
-actual_data = df[df['ds'] >= forecast['ds'].min()]
-actual_data = actual_data[actual_data['ds'] <= forecast['ds'].max()]
-
 # Fusionner les prévisions et les observations réelles sur la colonne 'ds'
-merged_data = pd.merge(actual_data, forecast, on='ds', how='inner')
+merged_data = pd.merge(df, forecast, on='ds', how='inner')
 
 # Calculer le RMSE
 rmse = round(np.sqrt(mean_squared_error(merged_data['y'], merged_data['yhat'])),1)
@@ -42,40 +34,15 @@ print(f"RMSE: {rmse}")
 print(f"MAE: {mae}")
 
 
-import pandas as pd
-from prophet import Prophet
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-import numpy as np
-from datetime import datetime
-import matplotlib.pyplot as plt
-
-# Data processing
-df = pd.read_csv('valeurs_mensuelles.csv', sep=";")
-df.describe()
-df.columns = ['ds', 'y']
-df['ds'] = pd.to_datetime(df['ds'])
-
-model = Prophet(interval_width=0.95)
-training_run = model.fit(df)
-
-future = model.make_future_dataframe(periods=25, freq='M')
-forecast = model.predict(future)
-
-# Plot
+# Visualisation des prédictions
 fig, ax = plt.subplots(figsize=(12, 6))
 
-# Plot the forecast
+# Prévisions
 model.plot(forecast, ax=ax)
-
-# Plot the original series
+# Série originale
 ax.plot(df['ds'], df['y'], color='red', label='Original Series')
 
-# Set labels and title
 ax.set_xlabel('Date')
 ax.set_ylabel('Number of created companies')
-
-# Add legend
 ax.legend()
-
-# Show the plot
 plt.show()
